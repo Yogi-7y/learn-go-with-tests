b97b61595a19cbdc9ca12177717bd549217d6686
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-05-13 21:01:08 +0530

    Restructure files

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("John")
+		got := Hello("John", "") // Pass an empty string as the second argument
 		want := "Hello, John!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "") // Pass an empty string as the second argument
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("John", "Spanish")
+		want := "Hola, John!"
 		assertCorrectMessage(t, got, want)
 	})
 }
