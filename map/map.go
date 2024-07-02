e7bee9277e8e799d53aed3dd408663d0efa4acf8
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-07-02 12:29:53 +0530

    refactor: Add Dictionary struct and Search method

---
+++ map/map.go
@@ -0,0 +1,17 @@
+package main
+
+import "errors"
+
+var ErrorNotFound = errors.New("could not find the word you were looking for")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrorNotFound
+	}
+
+	return definition, nil
+}
